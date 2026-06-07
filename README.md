be2c35b850c6c207d0f9882b3f07a69401e2ed2f
kodydeda4 <kodydeda4@gmail.com>
2026-06-07 01:58:50 -0400

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+My dotfiles.
+
+<img width="1280" alt="image" src="https://github.com/user-attachments/assets/1e7cb1a6-43f6-4cac-8edc-e4b08234ddc4" />
+
+```bash
+# git clone
+git clone https://github.com/kodydeda4/.dotfiles
+cd ~/.dotfiles
+
+# GNU Stow "config" into `~/.config`
+stow config --target="$HOME/.config"
+```
---
+++ config/raycast/raycast-scripts-kody/brave---open-profile-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Brave - Open Profile 1
+# @raycast.mode inline
+# @raycast.refreshTime 1h
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author b8dmv9hp8w_privaterelay_appleid_com
+# @raycast.authorURL https://raycast.com/b8dmv9hp8w_privaterelay_appleid_com
+
+open -na "Brave Browser" --args --profile-directory="Profile 1"
+
